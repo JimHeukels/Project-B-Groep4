c968493e5af7c11b37d9a63bdf62c1f526885809
OzzyOs <joshuahursepuny@gmail.com>
2020-03-18 11:41:55 +0100

    Genre/Beschrijving
    Genre, Beschrijving, Films.

---
+++ Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Movie
+    {
+        public string Name;
+        public string Genre;
+
+        public string getData()
+        {
+            return this.Name + " is a movie based on " + this.Genre + "!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> lstMovies = new List<Movie>();
+            lstMovies.Add(new Movie());
+            lstMovies[0].Name = "The Lord of the Rings";
+            lstMovies[0].Genre = "Adventure";
+
+            lstMovies.Add(new Movie());
+            lstMovies[1].Name = "The Hobbit";
+            lstMovies[1].Genre = "Adventure";
+
+            foreach (Movie oMovie in lstMovies)
+            {
+                Console.WriteLine(oMovie.getData());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
