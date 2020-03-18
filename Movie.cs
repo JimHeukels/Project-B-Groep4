f58670d604bdd5cf2d4e39b5f4b79519ae950ae7
OzzyOs <joshuahursepuny@gmail.com>
2020-03-18 11:53:11 +0100

    Updated Movie.cs
    Movie description change.

--- Movie.cs
+++ Movie.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,7 @@ namespace ConsoleApp1
 
         public string getData()
         {
-            return this.Name + " is a movie based on " + this.Genre + "!";
+            return this.Name + "\nis an " + this.Genre + " based movie!\n";
         }
     }
 }
